de98d50e694b2302fd13d8810b0f8dc9ad5379bd
RaffaBik <rafael.biktimirov2@aiesec.net>
2019-06-08 14:29:34 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const http = require ('http');
+const fs = require('fs'); //чтение файла
+
+const server = http.createServer(function (request, response) {
+    console.log(request.method, request.url);
+
+    if (request.url=='/style.css'){
+        const css = fs.readFileSync('style.css', 'utf8');
+        response.end(css);//закрытие запроса
+    }
+    else {
+        const html = fs.readFileSync('index.html', 'utf8');
+        response.end(html);//закрытие запроса
+    }
+
+
+});
+
+server.listen (3000); //звпуск сервера
+console.log('Server started !');
+
+//если в брауэере набрать localhost:3000 в терминале отобразиться hello
+//по фавикону браузер запрашивает иконку сайта
